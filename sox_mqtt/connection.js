980543973ce4f947b32ec46fef30e75ec4bc55ed
drgnman <drgnman@ht.sfc.keio.ac.jp>
2023-02-02 10:35:48 +0900

    keepaliveを設定可能にした

--- sox_mqtt/connection.js
+++ sox_mqtt/connection.js
@@ -1,9 +1,10 @@
 const { PublishModule } = require(".");
 
-function Connection(broker, port, username=null, password=null){
+function Connection(broker, port, keepalive=60, username=null, password=null){
     this.mqtt = require("mqtt");
     this.broker = broker;
     this.port = port;
+    this.keepalive = keepalive;
     this.username = username;
     this.password = password;
 }
@@ -17,6 +18,7 @@ Connection.prototype.connect = function(){
         } else {
             client.username = this.username;
             client.password = this.password;
+            client.keepalive = this.keepalive;
         }
     }
     return client;
