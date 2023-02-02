c9ddda817abbbff6c4f2579c27e0cca3f7eb6118
drgnman <drgnman@ht.sfc.keio.ac.jp>
2023-02-02 09:51:51 +0900

    getDay()をgetDate()に修正

--- sox_mqtt/node.js
+++ sox_mqtt/node.js
@@ -59,7 +59,7 @@ function Node(node_name){
             "raw_value": transducer.getRawValue(),
             "publish_timestamp": now.getFullYear() + "-" 
             + (now.getMonth()+1).toString().padStart(2, "0") + "-"
-            + now.getDay().toString().padStart(2, "0") + " "
+            + now.getDate().toString().padStart(2, "0") + " "
             + now.getHours().toString().padStart(2, "0") + ":"
             + now.getMinutes().toString().padStart(2, "0") + ":"
             + now.getSeconds().toString().padStart(2, "0") + "."
